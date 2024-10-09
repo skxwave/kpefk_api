29c397a6b8c58a908238aceaad911ddb18c8f5b5
Roman <cheepa30@gmail.com>
2024-10-09 14:08:43 +0300

    change settings location

--- backend/manage.py
+++ backend/manage.py
@@ -3,10 +3,20 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        os.getenv(
+            "DJANGO_SETTINGS_MODULE",
+            default="backend.settings.local",
+        ),
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
