a56f3d1457b317a1ac670fc749c3cfa7a7c571f8
Roman <cheepa30@gmail.com>
2024-10-09 14:11:58 +0300

    urls updated

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,8 +17,18 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/schedule/", include("apps.schedule.api.urls")),
+]
+
+urlpatterns += [
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
