8f11bb022ea8e1215ae6b1420b2c7ee6e4b3ef34
Roman <cheepa30@gmail.com>
2024-10-14 14:31:24 +0300

    Group info view

--- backend/apps/groups/api/urls.py
+++ backend/apps/groups/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import GetGroupsListAPIView
+from .views import GetGroupsListAPIView, GetGroupInfoAPIView
 
 
 urlpatterns = [
     path("", GetGroupsListAPIView.as_view()),
+    path("info/<group_number>", GetGroupInfoAPIView.as_view()),
 ]
--- backend/apps/groups/api/views.py
+++ backend/apps/groups/api/views.py
@@ -16,3 +16,28 @@ class GetGroupsListAPIView(APIView):
             },
             status=status.HTTP_200_OK,
         )
+
+
+class GetGroupInfoAPIView(APIView):
+    def get(self, request, group_number, *args, **kwargs):
+        with open("schedule.json", encoding="utf-8") as file:
+            data = json.load(file)
+
+        group = data.get("groups", {}).get(group_number)
+
+        if not group:
+            return Response(
+                data={
+                    "status": "NOT_FOUND",
+                    "result": "Group not found",
+                },
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        return Response(
+            data={
+                "status": "OK",
+                "result": group["info"],
+            },
+            status=status.HTTP_200_OK,
+        )
